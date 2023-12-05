e6cba28cde48cfde3df7dcf9606ea8da04af5fa5
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2023-12-05 18:39:06 +0000

    [RORO-4705][EL] - upgrade play-frontend-hmrc-v7.29.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-28" % "7.13.0",
     "org.typelevel" %% "cats-core"                  % "2.9.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc"         % "7.17.0-play-28"
+    "uk.gov.hmrc"   %% "play-frontend-hmrc"         % "7.29.0-play28"
   )
 
   val test = Seq(
