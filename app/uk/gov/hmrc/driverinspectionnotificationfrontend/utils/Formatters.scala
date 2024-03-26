4216d5357ab8bce0ee823d06938e592a0d38c5af
Ben Tipper <66072836+tipperba@users.noreply.github.com>
2024-03-26 15:42:44 +0000

    [RORO-4866][BT] switch inspection pending screen to GMT/BST (#19)

--- app/uk/gov/hmrc/driverinspectionnotificationfrontend/utils/Formatters.scala
+++ app/uk/gov/hmrc/driverinspectionnotificationfrontend/utils/Formatters.scala
@@ -18,19 +18,19 @@ package uk.gov.hmrc.driverinspectionnotificationfrontend.utils
 
 import play.api.i18n.Messages
 
-import java.time.{Instant, LocalDate, ZoneId}
 import java.time.format.DateTimeFormatter
+import java.time.{Instant, LocalDate, ZoneId}
 import java.util.Locale
 
 object Formatters {
 
-  private def zoneId = ZoneId.of("CET")
+  private def zoneId = ZoneId.of("Europe/London")
 
   def instantFormatDate(instant: Instant)(implicit messages: Messages): LocalDate = instant.atZone(zoneId).toLocalDate
 
   def instantFormatHours(instant: Instant)(implicit messages: Messages): String = hoursFormatter(messages.lang.locale).format(instant)
 
-  def hoursFormatter(locale: Locale): DateTimeFormatter =
+  private def hoursFormatter(locale: Locale): DateTimeFormatter =
     DateTimeFormatter
       .ofPattern("HH:mm z")
       .withLocale(locale)
